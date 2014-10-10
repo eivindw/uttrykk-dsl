35bdf426d99e67a44f20782a7a5fe0d91b933af5
eivindw <eivindw@gmail.com>
2014-10-10 23:05:01 +0200

    Static imports i testen.

--- src/test/java/ske/fastsetting/skatt/uttrykk/BasisTest.java
+++ src/test/java/ske/fastsetting/skatt/uttrykk/BasisTest.java
@@ -1,23 +1,23 @@
 package ske.fastsetting.skatt.uttrykk;
 
 import org.junit.Test;
-import ske.fastsetting.skatt.domene.Tall;
-import ske.fastsetting.skatt.uttrykk.belop.BelopSumUttrykk;
 import ske.fastsetting.skatt.uttrykk.belop.BelopUttrykk;
-import ske.fastsetting.skatt.uttrykk.belop.KroneUttrykk;
 import ske.fastsetting.skatt.uttrykk.tall.ProsentUttrykk;
 import ske.fastsetting.skatt.uttrykk.uttrykkbeskriver.ListUttrykkBeskriver;
+
+import static ske.fastsetting.skatt.uttrykk.belop.BelopSumUttrykk.sum;
+import static ske.fastsetting.skatt.uttrykk.belop.KroneUttrykk.kr;
+import static ske.fastsetting.skatt.uttrykk.tall.ProsentUttrykk.prosent;
 
 public class BasisTest {
 
    @Test
    public void prosentUttrykk() {
-      final ProsentUttrykk satsTrygdeavgift =
-         new ProsentUttrykk(Tall.prosent(8.2)).medNavn("Sats trygdeavgift");
+      final ProsentUttrykk satsTrygdeavgift = prosent(8.2).medNavn("Sats trygdeavgift");
 
-      final BelopUttrykk sumLonn = BelopSumUttrykk.sum(
-         KroneUttrykk.kr(60_000).medNavn("Lønn"),
-         KroneUttrykk.kr(40_000).medNavn("Bonus")
+      final BelopUttrykk sumLonn = sum(
+          kr(60_000).medNavn("Lønn"),
+          kr(40_000).medNavn("Bonus")
       ).medNavn("Sum lønn");
 
       final BelopUttrykk trygdeavgift = sumLonn.multiplisertMed(satsTrygdeavgift);
