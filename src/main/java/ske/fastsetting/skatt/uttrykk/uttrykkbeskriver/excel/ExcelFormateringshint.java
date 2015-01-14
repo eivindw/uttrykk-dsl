c9804de651002d0ee74cd9a193482f7be55821de
Jørn Ola Birkeland <jornola.birkeland@gmail.com>
2015-01-14 08:40:28 +0100

    Legger til formatteringshint, slik at formler kan få riktig numerisk format del 2

---
+++ src/main/java/ske/fastsetting/skatt/uttrykk/uttrykkbeskriver/excel/ExcelFormateringshint.java
@@ -0,0 +1,33 @@
+package ske.fastsetting.skatt.uttrykk.uttrykkbeskriver.excel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jorn ola birkeland on 14.01.15.
+ */
+public class ExcelFormateringshint {
+    public static final String BELOP_FORMATERING = "kr ###,###,###,##0";
+    public static final String PROSENT_FORMATERING = "0.00%";
+    private final Map<String,String> patternFormatMap = new HashMap<>();
+    private String standardFormat = "";
+
+    public void leggTilHint(String navnPattern, String excelFormat) {
+        patternFormatMap.put(navnPattern,excelFormat);
+    }
+
+    public void settStandardFormatering(String excelFormat) {
+        standardFormat = excelFormat;
+    }
+
+    public String finnFormat(String navn) {
+        for(String pattern : patternFormatMap.keySet() ) {
+            if(navn.matches(pattern)) {
+                return patternFormatMap.get(pattern);
+            }
+        }
+
+        return standardFormat;
+    }
+
+}
