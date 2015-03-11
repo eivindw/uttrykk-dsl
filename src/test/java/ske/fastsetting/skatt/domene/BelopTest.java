39f565f34bde9e4ac4f769f2ffc3cc2e7d91f5a3
Eivind B Waaler <eivindw@gmail.com>
2015-03-11 14:18:11 +0100

    SOF-134: Start på noen tester for beløp.

--- src/test/java/ske/fastsetting/skatt/domene/BelopTest.java
+++ src/test/java/ske/fastsetting/skatt/domene/BelopTest.java
@@ -2,7 +2,10 @@ package ske.fastsetting.skatt.domene;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.*;
+import static ske.fastsetting.skatt.domene.Belop.kr;
 
 public class BelopTest {
 
@@ -13,7 +16,34 @@ public class BelopTest {
         assertDoubleToBelop(11, 10.50);
     }
 
+    @Test
+    public void avrunding() {
+        assertBelop(125, kr(123).rundAvTilNaermeste(5));
+        assertBelop(120, kr(123).rundAvTilNaermeste(10));
+        assertBelop(100, kr(123).rundAvTilNaermeste(100));
+        assertBelop(0, kr(123).rundAvTilNaermeste(1000));
+    }
+
+    @Test
+    public void forholdstall() {
+        final BigDecimal forhold = kr(2).dividertMed(kr(3));
+
+        assertEquals("0.6666666666666666", forhold.toPlainString());
+    }
+
+    @Test
+    public void divisjonMultiplikasjon() {
+        final Belop belop = kr(5).dividertMed(2);
+
+        assertBelop(3, belop);
+        assertBelop(5, belop.multiplisertMed(BigDecimal.valueOf(2)));
+    }
+
     public void assertDoubleToBelop(int forventet, double input) {
-        assertEquals(Integer.valueOf(forventet), Belop.kr(input).toInteger());
+        assertBelop(forventet, kr(input));
+    }
+
+    public void assertBelop(int forventet, Belop belop) {
+        assertEquals(Integer.valueOf(forventet), belop.toInteger());
     }
 }
