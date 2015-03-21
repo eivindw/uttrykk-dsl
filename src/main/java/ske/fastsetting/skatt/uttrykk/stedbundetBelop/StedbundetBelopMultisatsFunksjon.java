5b09d3405addbcd0a1fe6fcc8594d73a70500ac1
Jørn Ola Birkeland <jornola.birkeland@gmail.com>
2015-03-21 16:04:58 +0100

    Generaliserer grenseuttrykk

--- src/main/java/ske/fastsetting/skatt/uttrykk/stedbundetBelop/StedbundetBelopMultisatsFunksjon.java
+++ src/main/java/ske/fastsetting/skatt/uttrykk/stedbundetBelop/StedbundetBelopMultisatsFunksjon.java
@@ -11,7 +11,7 @@ import ske.fastsetting.skatt.uttrykk.belop.BelopDiffUttrykk;
 import java.util.Collection;
 
 import static ske.fastsetting.skatt.uttrykk.belop.KroneUttrykk.kr;
-import static ske.fastsetting.skatt.uttrykk.stedbundetBelop.StebundetGrenseUttrykk.begrens;
+import static ske.fastsetting.skatt.uttrykk.stedbundetBelop.StebundetGrenseUttrykk.begrensFordholdmessig;
 import static ske.fastsetting.skatt.uttrykk.stedbundetBelop.TilStedbundetBelopUttrykk.tilStedbundetBelopUttrykk;
 
 public class StedbundetBelopMultisatsFunksjon<K>
@@ -32,10 +32,10 @@ public class StedbundetBelopMultisatsFunksjon<K>
         final SatsStegUttrykk<StedbundetBelop<K>, StedbundetBelop<K>, Tall, Belop> satsStegUttrykk = new SatsStegUttrykk<StedbundetBelop<K>, StedbundetBelop<K>, Tall, Belop>() {
             @Override
             public StedbundetBelop<K> eval(UttrykkContext ctx) {
-                StebundetGrenseUttrykk<K> grenseUttrykk = begrens(new ProporsjonalFordelingDiffUttrykk<>(grunnlag, nedreGrense).multiplisertMed(sats))
-                        .nedadProporsjonalt(kr(0));
+                StebundetGrenseUttrykk<K> grenseUttrykk = begrensFordholdmessig(new ProporsjonalFordelingDiffUttrykk<>(grunnlag, nedreGrense).multiplisertMed(sats))
+                        .nedad(kr(0));
                 if (oevreGrense != null) {
-                    grenseUttrykk.oppadProporsjonalt(new BelopDiffUttrykk(oevreGrense, nedreGrense).multiplisertMed(sats));
+                    grenseUttrykk.oppad(new BelopDiffUttrykk(oevreGrense, nedreGrense).multiplisertMed(sats));
                 }
 
                 return ctx.eval(grenseUttrykk);
