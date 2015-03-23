7728a53cca608c2e3391141719a0ca252001c336
Jørn Ola Birkeland <jornola.birkeland@skatteetaten.no>
2015-03-23 10:56:09 +0100

    Utvider Distanse med static inits

--- src/main/java/ske/fastsetting/skatt/uttrykk/distanse/Distanse.java
+++ src/main/java/ske/fastsetting/skatt/uttrykk/distanse/Distanse.java
@@ -4,8 +4,16 @@ import ske.fastsetting.skatt.domene.KalkulerbarVerdi;
 import ske.fastsetting.skatt.domene.Kvantitet;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class Distanse extends Kvantitet<Double,Kilometer> implements Comparable<Distanse>, KalkulerbarVerdi<Distanse> {
+
+    public static Distanse NULL = new Distanse(0d);
+
+    public static Distanse km(BigInteger km) { return new Distanse(km.doubleValue());}
+    public static Distanse km(BigDecimal km) { return new Distanse(km.doubleValue());}
+    public static Distanse km(double km) { return new Distanse(km);}
+    public static Distanse km(long km) { return new Distanse((double) km);}
 
     public Distanse(Double verdi) {
         super(verdi);
