0a44e48f461219909c26f23aac35e361376539aa
Jørn Ola Birkeland <jornola.birkeland@skatteetaten.no>
2015-04-01 10:48:30 +0200

    Fikser litt for agressiv package-skoping

--- src/test/java/ske/fastsetting/skatt/uttrykk/skalSlettes/BeregningsgrunnlagTallUttrykk.java
+++ src/test/java/ske/fastsetting/skatt/uttrykk/skalSlettes/BeregningsgrunnlagTallUttrykk.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 import ske.fastsetting.skatt.domene.Tall;
 import ske.fastsetting.skatt.uttrykk.tall.TallUttrykk;
 
-class BeregningsgrunnlagTallUttrykk extends Beregningsgrunnlag.BeregningsgrunnlagUttrykk<Tall, TallUttrykk,
+public class BeregningsgrunnlagTallUttrykk extends Beregningsgrunnlag.BeregningsgrunnlagUttrykk<Tall, TallUttrykk,
   BeregningsgrunnlagTallUttrykk> implements TallUttrykk {
 
     public static BeregningsgrunnlagTallUttrykk beregningsgrunnlag(Function<Beregningsgrunnlag, TallUttrykk> function) {
