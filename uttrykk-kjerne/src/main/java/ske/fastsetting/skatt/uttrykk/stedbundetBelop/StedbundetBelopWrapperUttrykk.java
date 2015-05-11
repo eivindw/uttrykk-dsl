10208342a028557fcc7e1abbe8cd666b3ef4e27e
Jørn Ola Birkeland <jornola.birkeland@skatteetaten.no>
2015-05-11 16:04:27 +0200

    Lager pakkInn-funksjonalitet for stedbundet uttrykk

--- uttrykk-kjerne/src/main/java/ske/fastsetting/skatt/uttrykk/stedbundetBelop/StedbundetBelopWrapperUttrykk.java
+++ uttrykk-kjerne/src/main/java/ske/fastsetting/skatt/uttrykk/stedbundetBelop/StedbundetBelopWrapperUttrykk.java
@@ -12,9 +12,13 @@ import java.util.stream.Collectors;
 <<<<<<< HEAD:src/main/java/ske/fastsetting/skatt/uttrykk/stedbundetBelop/TilStedbundetBelopUttrykk.java
  * Created by jorn ola birkeland on 21.03.15.
  */
-class StedbundetBelopWrapperUttrykk<K> extends AbstractUttrykk<StedbundetBelop<K>, StedbundetBelopWrapperUttrykk<K>>
+public class StedbundetBelopWrapperUttrykk<K> extends AbstractUttrykk<StedbundetBelop<K>, StedbundetBelopWrapperUttrykk<K>>
   implements StedbundetBelopUttrykk<K> {
     private Uttrykk<StedbundetBelop<K>> uttrykk;
+
+    public static <K>StedbundetBelopWrapperUttrykk<K> pakkInn(Uttrykk<StedbundetBelop<K>> uttrykk) {
+        return new StedbundetBelopWrapperUttrykk<>(uttrykk);
+    }
 
     public static <T> Collection<StedbundetBelopUttrykk<T>> tilStedbundetBelopUttrykk
       (Collection<Uttrykk<StedbundetBelop<T>>> uttrykk) {
@@ -27,6 +31,7 @@ class StedbundetBelopWrapperUttrykk<K> extends AbstractUttrykk<StedbundetBelop<K
 
     public StedbundetBelopWrapperUttrykk(Uttrykk<StedbundetBelop<K>> uttrykk) {
         this.uttrykk = uttrykk;
+        navn(uttrykk.navn());
     }
 
     @Override
