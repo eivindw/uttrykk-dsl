f25189699e418754a2c525a0fd219a601e2eab9e
Jørn Ola Birkeland <jornola.birkeland@gmail.com>
2015-05-12 08:36:32 +0200

    Glemte visst noen filer

---
+++ uttrykk-kjerne/src/test/java/ske/fastsetting/skatt/uttrykk/skalSlettes/StubTest.java
@@ -0,0 +1,32 @@
+package ske.fastsetting.skatt.uttrykk.skalSlettes;
+
+import org.junit.Test;
+import ske.fastsetting.skatt.domene.Belop;
+import ske.fastsetting.skatt.uttrykk.belop.BelopUttrykk;
+import ske.fastsetting.skatt.uttrykk.test.StubUttrykkContext;
+
+import static ske.fastsetting.skatt.uttrykk.belop.BelopSumUttrykk.sum;
+import static ske.fastsetting.skatt.uttrykk.belop.KroneUttrykk.kr;
+import static ske.fastsetting.skatt.uttrykk.tall.ProsentUttrykk.prosent;
+
+/**
+ * Created by jorn ola birkeland on 11.05.15.
+ */
+public class StubTest {
+
+    private final static BelopUttrykk loenn = kr(25);
+    private final static BelopUttrykk renteinntekt = kr(12);
+
+    private final static BelopUttrykk alminneligInntekt = sum(loenn,renteinntekt);
+
+    private final static BelopUttrykk fellesskatt = alminneligInntekt.multiplisertMed(prosent(13.5));
+
+    @Test
+    public void test() {
+        StubUttrykkContext kontekst = StubUttrykkContext.ny();
+
+        kontekst.stub(alminneligInntekt, Belop.kr(200));
+
+        System.out.println(kontekst.verdiAv(fellesskatt));
+    }
+}
