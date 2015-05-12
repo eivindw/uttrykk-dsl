9dc44066e1789f05758071e8b31302404117c9e4
Jørn Ola Birkeland <jornola.birkeland@skatteetaten.no>
2015-05-12 12:07:28 +0200

    Justerer på stub-eksempel

--- uttrykk-kjerne/src/test/java/ske/fastsetting/skatt/uttrykk/skalSlettes/StubTest.java
+++ uttrykk-kjerne/src/test/java/ske/fastsetting/skatt/uttrykk/skalSlettes/StubTest.java
@@ -1,13 +1,14 @@
 package ske.fastsetting.skatt.uttrykk.skalSlettes;
 
-import org.junit.Test;
-import ske.fastsetting.skatt.domene.Belop;
-import ske.fastsetting.skatt.uttrykk.belop.BelopUttrykk;
-import ske.fastsetting.skatt.uttrykk.test.StubUttrykkContext;
-
 import static ske.fastsetting.skatt.uttrykk.belop.BelopSumUttrykk.sum;
 import static ske.fastsetting.skatt.uttrykk.belop.KroneUttrykk.kr;
 import static ske.fastsetting.skatt.uttrykk.tall.ProsentUttrykk.prosent;
+
+import org.junit.Test;
+
+import ske.fastsetting.skatt.domene.Belop;
+import ske.fastsetting.skatt.uttrykk.belop.BelopUttrykk;
+import ske.fastsetting.skatt.uttrykk.test.StubUttrykkContext;
 
 /**
  * Created by jorn ola birkeland on 11.05.15.
@@ -23,10 +24,13 @@ public class StubTest {
 
     @Test
     public void test() {
+        // Benytt orignalt uttrykk for alminnelig inntekt
         StubUttrykkContext kontekst = StubUttrykkContext.ny();
-
-        kontekst.stub(alminneligInntekt, Belop.kr(200));
-
         System.out.println(kontekst.verdiAv(fellesskatt));
+
+        // Benytt stub'et verdi for alminnelig inntekt
+        StubUttrykkContext stubKontekst = StubUttrykkContext.ny();
+        stubKontekst.stub(alminneligInntekt, Belop.kr(200));
+        System.out.println(stubKontekst.verdiAv(fellesskatt));
     }
 }
