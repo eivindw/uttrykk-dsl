b4c2d8a105cadce2c97b01715558eb654612fea8
Jørn Ola Birkeland <jornola.birkeland@gmail.com>
2015-06-09 07:26:12 +0200

    Legger til uttrykksbeskriver for Neo4J

---
+++ uttrykk-beskriver/src/main/java/ske/fastsetting/skatt/uttrykk/uttrykkbeskriver/neo4j/rest/Value.java
@@ -0,0 +1,10 @@
+package ske.fastsetting.skatt.uttrykk.uttrykkbeskriver.neo4j.rest;
+
+/**
+ * Created by jorn ola birkeland on 08.06.15.
+ */
+public class Value {
+    public static Object string(String value) {
+        return "\""+value+"\"";
+    }
+}
