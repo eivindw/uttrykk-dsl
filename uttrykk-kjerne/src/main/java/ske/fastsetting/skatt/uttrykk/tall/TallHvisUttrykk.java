162cf9dc929e8e4019ef878560ebcd84ba7c5c03
Marie Beate Forsdahl <marie.forsdahl@skatteetaten.no>
2015-09-02 11:32:45 +0200

    Generaliserer hvis-uttrykk til å bruke Uttrykk<Boolean> og ikke BolskUttrykk

--- uttrykk-kjerne/src/main/java/ske/fastsetting/skatt/uttrykk/tall/TallHvisUttrykk.java
+++ uttrykk-kjerne/src/main/java/ske/fastsetting/skatt/uttrykk/tall/TallHvisUttrykk.java
@@ -1,12 +1,13 @@
 package ske.fastsetting.skatt.uttrykk.tall;
 
 import ske.fastsetting.skatt.domene.Tall;
+import ske.fastsetting.skatt.uttrykk.Uttrykk;
 import ske.fastsetting.skatt.uttrykk.bolsk.BolskUttrykk;
 import ske.fastsetting.skatt.uttrykk.HvisUttrykk;
 
 public class TallHvisUttrykk extends HvisUttrykk<Tall, TallHvisUttrykk> implements TallUttrykk {
 
-    public static BrukUttrykk<Tall, TallHvisUttrykk> hvis(BolskUttrykk bolskUttrykk) {
+    public static BrukUttrykk<Tall, TallHvisUttrykk> hvis(Uttrykk<Boolean> bolskUttrykk) {
         return new BrukUttrykk<>(bolskUttrykk, new TallHvisUttrykk());
     }
 
--- uttrykk-kjerne/src/main/java/ske/fastsetting/skatt/uttrykk/tekst/TekstHvisUttrykk.java
+++ uttrykk-kjerne/src/main/java/ske/fastsetting/skatt/uttrykk/tekst/TekstHvisUttrykk.java
@@ -1,10 +1,13 @@
 package ske.fastsetting.skatt.uttrykk.tekst;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import ske.fastsetting.skatt.uttrykk.Uttrykk;
 import ske.fastsetting.skatt.uttrykk.bolsk.BolskUttrykk;
 import ske.fastsetting.skatt.uttrykk.HvisUttrykk;
 
 public class TekstHvisUttrykk extends HvisUttrykk<String, TekstHvisUttrykk> implements TekstUttrykk {
-    public static BrukUttrykk<String, TekstHvisUttrykk> hvis(BolskUttrykk bolskUttrykk) {
+    public static BrukUttrykk<String, TekstHvisUttrykk> hvis(Uttrykk<Boolean> bolskUttrykk) {
         return new BrukUttrykk<>(bolskUttrykk, new TekstHvisUttrykk());
     }
 }
