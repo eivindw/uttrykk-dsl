2c8d3b3209a9073979c1e728fd35f1f670e0838a
Jørn Ola Birkeland <jornola.birkeland@skatteetaten.no>
2015-09-10 14:32:07 +0200

    Legger til et par pakkInn-uttrykk

---
+++ uttrykk-kjerne/src/main/java/ske/fastsetting/skatt/uttrykk/bolsk/TilBolskUttrykk.java
@@ -0,0 +1,38 @@
+package ske.fastsetting.skatt.uttrykk.bolsk;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import ske.fastsetting.skatt.uttrykk.Uttrykk;
+import ske.fastsetting.skatt.uttrykk.UttrykkContext;
+
+public class TilBolskUttrykk extends BolskUttrykk {
+    private Uttrykk<Boolean> uttrykk;
+
+    public static Collection<BolskUttrykk> tilBolskUttrykk(Collection<Uttrykk<Boolean>> uttrykk) {
+        return uttrykk.stream().map(TilBolskUttrykk::tilTalUttrykk).collect(Collectors.toList());
+    }
+
+    public static TilBolskUttrykk tilTalUttrykk(Uttrykk<Boolean> uttrykk) {
+        return new TilBolskUttrykk(uttrykk);
+    }
+
+    public static TilBolskUttrykk pakkInn(Uttrykk<Boolean> uttrykk) {
+        return new TilBolskUttrykk(uttrykk);
+    }
+
+    public TilBolskUttrykk(Uttrykk<Boolean> uttrykk) {
+        this.uttrykk = uttrykk;
+        navn(uttrykk.navn());
+    }
+
+    @Override
+    public Boolean eval(UttrykkContext ctx) {
+        return uttrykk.eval(ctx);
+    }
+
+    @Override
+    public String beskriv(UttrykkContext ctx) {
+        return uttrykk.beskriv(ctx);
+    }
+}
